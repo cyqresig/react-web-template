b69c6c24b2558cec600b6ba50e6c60d35f222936
cyqresig <moonsunfall@aliyun.com>
2017-04-03 15:44:08 +0800

    test eslint rules

---
+++ test/test.js
@@ -0,0 +1,19 @@
+/**
+ * @since 2017-04-03 15:07
+ * @author chenyiqin
+ */
+
+
+let test = 1;
+
+let obj = {
+    test: 1,
+}
+
+obj.test = test
+
+if (1) {
+    console.log(2)
+} else {
+    console.log(2)
+}
